5ec748b338c800fbd749089a503e1baf44f7adfa
haifeiWu <1253242444@qq.com>
2019-02-05 19:04:16 +0800

    add new protocol

---
+++ child-rpc-core/src/main/java/cn/whforever/core/protocol/bolt/server/BoltServer.java
@@ -0,0 +1,15 @@
+package cn.whforever.core.protocol.bolt.server;
+
+import cn.whforever.core.config.Config;
+
+public class BoltServer extends ChildServer {
+    @Override
+    public void start(Config config) throws Exception {
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+
+    }
+}
